0730f9e24ae54554f8ffec157e1f0d0ecac54f66
hakobk <hakobk1@gmail.com>
2015-04-22 17:58:00 +0200

    fackit

--- SteamMetro/SteamMetro/MainPage.xaml.cs
+++ SteamMetro/SteamMetro/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using SteamSharp;
 using Newtonsoft;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -32,7 +33,29 @@ namespace SteamMetro
         public async void getInfo()
         {
             SteamClient client = new SteamClient();
+            string build = "";
+            int count = 0;
+            //List<SteamUserStats.GlobalAchievement> list = new List<SteamUserStats.GlobalAchievement>();
+            //list = await SteamUserStats.GetGlobalAchievementPercentagesForAppAsync(client,740);
+            
+            //PlayerRelationshipType s = new PlayerRelationshipType();
+            //List<SteamUserInterface.Friend> ggg = SteamUserInterface.GetFriendList(client, "76561198035450677", s);
 
+            SteamNews.AppNews news =await SteamNews.GetNewsForAppAsync(client, 740, 6, 100);
+            
+
+            //var s = Newtonsoft.Json.JsonConvert.DeserializeObject<List<SteamUserStats.GlobalAchievement>>(list,null);
+        
+                //MessageDialog message = new MessageDialog(d.SteamID.ToString());
+            foreach(SteamNews.NewsItem s in news.NewsItems)
+            {
+                build = build + " " + (news.AppID.ToString() + " en verdere informatie: " + s.Author.ToString() + " beschrijving: " + s.Contents.ToString());
+                count++;
+            }
+
+            MessageDialog message = new MessageDialog(news.AppID.ToString() + " " + count.ToString());
+            message.ShowAsync();
+            
         }
 
         private void Title_Copy1_SelectionChanged(object sender, RoutedEventArgs e)
@@ -42,8 +65,7 @@ namespace SteamMetro
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            SteamClient client = new SteamClient();
-
+            getInfo();
         }
     }
 }
